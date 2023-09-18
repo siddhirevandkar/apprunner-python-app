68274f614ee7869ea0444421e35f392916cc5f2b
siddhirevandkar <142626012+siddhirevandkar@users.noreply.github.com>
2023-09-18 18:02:14 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,1 +1,9 @@
-print("This is Python Application displaying Hello World!")
+from flask import Flask, jsonify, request, render_template
+from pprint import pprint
+from http import HttpsResponse
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+def index(request):
+  return HttpsResponse("This is Python Application displaying Hello World!")
